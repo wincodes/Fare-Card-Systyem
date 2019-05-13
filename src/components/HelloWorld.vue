70d52016c4d0002f2f84da03de1117d2322ba7ea
odafe1 <godwin.otokina@ikooba.com>
2019-05-13 14:36:17 +0100

    Done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <nav class="navbar fixed-top navbar-light bg-light">
+      <strong >Fare Card System</strong>
+      <strong v-if="balance">Card Balance: ${{balance}}</strong>
+    </nav>
+    <div class="row">
+      <div class="card  container col-6" padding="20px">
+        <h3>The Fare Card System</h3>
+        <hr>
+        <div class="card-body">
+          <h5 class="card-title">Click the Button below to Load your Card with $30</h5>
+          <button v-on:click="loadCard()" class="btn btn-primary">Load Card</button>
+          <hr>
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Welcome to 5th Station (Zone 1)</h5> 
+              <P>Click the Button Below to Check In</p>
+              <button :disabled='disable' v-on:click="checkIn($event)" class="btn btn-primary" name="5th">Check In</button>
+              <hr>
+            </div>
+          </div>
+
+          <div class="card">
+             <div class="card-body">
+                <h5 class="card-title">Welcome to Pelham	Parkway Station (Zone 1)</h5>
+                <p class="card-text">Click the Button Below to Check In</p>
+                <button :disabled='disable' v-on:click="checkIn($event)"  class="btn btn-primary" name="pelham">Check In</button>
+                <hr>
+                <p class="card-text">Click the Button Below to Check Out</p>
+                <button :disabled='disable2' v-on:click="checkOut($event)" class="btn btn-primary" name="pelham">Check Out</button>
+                <hr>
+              </div>
+          </div>
+          <hr>
+          <div class="card">
+             <div class="card-body">
+                <h5 class="card-title">Welcome to Pelham	Parkway Station (Zone 2)</h5>
+                <p class="card-text">Click the Button Below to Check In</p>
+                <button :disabled='disable' v-on:click="checkIn($event)" class="btn btn-primary" name="pelham">Check In</button>
+                <hr>
+                <p class="card-text">Click the Button Below to Check Out</p>
+                <button :disabled='disable2' v-on:click="checkOut($event)" class="btn btn-primary" name="pelham">Check Out</button>
+                <hr>
+              </div>
+          </div>
+          <hr>
+          <div class="card">
+             <div class="card-body">
+                <h5 class="card-title">Welcome to Bronx Station (Zone 3)</h5>
+                <p class="card-text">Click the Button Below to Check In</p>
+                <button  :disabled='disable' v-on:click="checkIn($event)" class="btn btn-primary" name="bronx">Check In</button>
+                <hr>
+                <p class="card-text">Click the Button Below to Check Out</p>
+                <button :disabled='disable2' v-on:click="checkOut($event)" class="btn btn-primary" name="bronx">Check Out</button>
+                <hr>
+              </div>
+          </div>
+          <hr>
+           <div class="card">
+             <div class="card-body">
+                <h5 class="card-title">Welcome to Guns Hill Station (Zone 2)</h5>
+                <p class="card-text">Click the Button Below to Check In</p>
+                <button :disabled='disable' v-on:click="checkIn($event)" class="btn btn-primary" name="guns">Check In</button>
+                <hr>
+                <p class="card-text">Click the Button Below to Check Out</p>
+                <button :disabled='disable2' v-on:click="checkOut($event)" class="btn btn-primary" name="guns">Check Out</button>
+                <hr>
+              </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,24 +81,50 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      startAt: '',
+      stoppedAt:'',
+      balance:'',
+      disable: true,
+      disable2: true
+    }
+  },
+  methods:{
+    loadCard(){
+        this.disable = false;
+        if(this.balance == ''){
+        this.balance = 30;
+        }else
+        {
+          this.balance = this.balance + 30;
+        }
+    },
+    checkIn(event){
+      this.disable = true
+      this.disable2 = false
+      this.startAt = event.target.name
+      this.stoppedAt = ''
+      this.balance = parseInt(this.balance) - 3.20;
+    },
+    checkOut(event){
+      this.disable = false
+      this.stoppedAt = event.target.name
+      this.calculateBalance()
+    },
+    calculateBalance(){
+      if(this.startAt == '5th' && this.stoppedAt == 'pelham'){
+        this.balance = this.balance + (3.20 - 2.5);
+      }
+      if(this.startAt == 'pelham' && this.stoppedAt == 'bronx'){
+        this.balance = this.balance + (3.20 - 1.80);
+      }
+      if(this.startAt == 'pelham' && this.stoppedAt == 'guns'){
+        this.balance = this.balance + (3.20 - 2.0);
+      }
+        
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
